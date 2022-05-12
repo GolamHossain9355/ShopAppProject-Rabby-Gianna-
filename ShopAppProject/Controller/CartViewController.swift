4e45662f6155d492b87f5ba3aac1f0502365dad2
developer <developer@developers-air.mynetworksettings.com>
2022-05-12 15:08:54 -0400

    cart loaded

---
+++ ShopAppProject/Controller/CartViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CartViewController.swift
+//  ShopAppProject
+//
+//  Created by developer on 5/12/22.
+//
+
+import UIKit
+//buy button delegates "Saved Items" back to product view controller
+
+class CartViewController: UIViewController {
+    @IBOutlet weak var cartTableView: UITableView!
+   
+    var cartItems: [ProductCellViewModel] = [ProductCellViewModel]() {
+        didSet {
+            cartTableView.reloadData()
+        }
+    }
+    
+    var cartTotal: Double {
+        var total = 0.0
+        for item in cartItems {
+            total += Double(item.price) ?? 0
+        }
+        return total
+    }
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        print("in cart")
+        cartTableView.dataSource = self
+        cartTableView.register(ProductTableViewCell.getNib(), forCellReuseIdentifier: ProductTableViewCell.identifier)
+        cartTableView.register(CartTotalTableViewCell.getNib(), forCellReuseIdentifier: CartTotalTableViewCell.identifier)
+    }
+    
+    
+}
+
+extension CartViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cartItems.count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == cartItems.count {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CartTotalTableViewCell.identifier, for: indexPath) as? CartTotalTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(price: self.cartTotal)
+            return cell
+        }
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ProductTableViewCell.identifier, for: indexPath) as? ProductTableViewCell else {
+            return UITableViewCell()
+        }
+        let cvm = cartItems[indexPath.row]
+        cell.configure(cellVM: cvm)
+        return cell
+    }
+    
+    
+}
